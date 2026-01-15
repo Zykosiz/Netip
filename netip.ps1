9bb24cca9f1a2796fdeb394ef96cf436e6198fb1
Zykosiz <zulanu@protonmail.com>
2026-01-15 08:30:59 +0100

    Update netip.ps1

--- netip.ps1
+++ netip.ps1
@@ -6,7 +6,7 @@ Repair-WinGetPackageManager
 Install-Module -Name PSWindowsUpdate -Force
 winget install Google.Chrome --accept-source-agreements
 winget install -e --id Adobe.Acrobat.Reader.64-bit --accept-source-agreements
-winget install -e --id Lenovo.Vantage --accept-source-agreements
+winget install -e --id Lenovo.SystemUpdate --accept-source-agreements
 $DownloadUrl = 'https://get.teamviewer.com/6nsd5xz'
 $Filename = $DownloadUrl | Split-Path -Leaf
 $DownloadPath = "$env:userprofile\desktop"
@@ -22,3 +22,4 @@ Restart-Computer -Force
 #choco install googlechrome --ignore-checksums
 #Invoke-WebRequest "https://get.teamviewer.com/6nsd5xz" -OutFile "C:\Temp\Netip.exe"
 #Move-Item -Path "C:\Temp\Netip.exe" -Destination ""$env:userprofile\desktop\netip.exe"" -Force
+
