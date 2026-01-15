15a39d4582896547129c4b88f0340d9ae9e6ca66
Zykosiz <zulanu@protonmail.com>
2026-01-15 14:00:52 +0100

    Update netip.ps1

--- netip.ps1
+++ netip.ps1
@@ -9,21 +9,21 @@ winget settings --enable InstallerHashOverride
 winget install Google.Chrome --accept-source-agreements --ignore-security-hash
 winget install -e --id Adobe.Acrobat.Reader.64-bit --accept-source-agreements
 winget install -e --id Lenovo.SystemUpdate --accept-source-agreements
-#$DownloadUrl = 'https://get.teamviewer.com/6nsd5xz'
-#$Filename = $DownloadUrl | Split-Path -Leaf
-#$DownloadPath = "$env:userprofile\desktop"
-#$WebClient = New-Object Net.WebClient
-#$WebClient.DownloadFile($DownloadUrl, $DownloadPath)
 Import-Module -Name PSWindowsUpdate
 Get-WindowsUpdate
+Start-Sleep -Seconds 15
 Install-WindowsUpdate -AcceptAll
-#Restart-Computer -Force
+Start-Sleep -Seconds 15
+Install-WindowsUpdate -AcceptAll
+Start-Sleep -Seconds 30
+Restart-Computer -Force
 
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
 #choco feature enable -n=allowGlobalConfirmation
 #choco install googlechrome --ignore-checksums
 #Invoke-WebRequest "https://get.teamviewer.com/6nsd5xz" -OutFile "C:\Temp\Netip.exe"
 #Move-Item -Path "C:\Temp\Netip.exe" -Destination ""$env:userprofile\desktop\netip.exe"" -Force
+
 
 
 
