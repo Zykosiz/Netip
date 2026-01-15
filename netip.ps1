010358ae6020c88e761b08122e1b0ac1ddc55892
Zykosiz <zulanu@protonmail.com>
2026-01-15 14:43:52 +0100

    Update netip.ps1

--- netip.ps1
+++ netip.ps1
@@ -4,6 +4,26 @@ choco feature enable -n=allowGlobalConfirmation
 choco install googlechrome --ignore-checksums
 choco install lenovo-thinkvantage-system-update --ignore-checksums
 choco install adobereader --ignore-checksums
+Install-Module -Name LSUClient
+Import-Module -Name LSUClient
+$updates = Get-LSUpdate | Where-Object { $_.Installer.Unattended }
+$updates | Save-LSUpdate -Verbose
+$updates | Install-LSUpdate -Verbose
+Start-Sleep -Seconds 15
+Import-Module -Name PSWindowsUpdate
+Get-WindowsUpdate
+Start-Sleep -Seconds 30
+Install-WindowsUpdate -AcceptAll
+Start-Sleep -Seconds 5
+Restart-Computer -Force
+
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+#choco feature enable -n=allowGlobalConfirmation
+#choco install googlechrome --ignore-checksums
+#Invoke-WebRequest "https://get.teamviewer.com/6nsd5xz" -OutFile "C:\Temp\Netip.exe"
+#Move-Item -Path "C:\Temp\Netip.exe" -Destination ""$env:userprofile\desktop\netip.exe"" -Force
+
+
 #Install-PackageProvider -Name NuGet -Force | Out-Null
 #Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 #Repair-WinGetPackageManager
@@ -13,18 +33,7 @@ choco install adobereader --ignore-checksums
 #winget install Google.Chrome --accept-source-agreements --ignore-security-hash
 #winget install -e --id Adobe.Acrobat.Reader.64-bit --accept-source-agreements
 #winget install -e --id Lenovo.SystemUpdate --accept-source-agreements
-Import-Module -Name PSWindowsUpdate
-Get-WindowsUpdate
-Start-Sleep -Seconds 15
-Install-WindowsUpdate -AcceptAll
-Start-Sleep -Seconds 30
-Restart-Computer -Force
 
-#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
-#choco feature enable -n=allowGlobalConfirmation
-#choco install googlechrome --ignore-checksums
-#Invoke-WebRequest "https://get.teamviewer.com/6nsd5xz" -OutFile "C:\Temp\Netip.exe"
-#Move-Item -Path "C:\Temp\Netip.exe" -Destination ""$env:userprofile\desktop\netip.exe"" -Force
 
 
 
