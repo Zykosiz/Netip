db9f06fb90bff4cb5e9e7520c8cdda0f7401f724
Zykosiz <zulanu@protonmail.com>
2026-01-15 11:38:24 +0100

    Update netip.ps1

--- netip.ps1
+++ netip.ps1
@@ -4,6 +4,7 @@ Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 Repair-WinGetPackageManager
 Install-Module -Name PSWindowsUpdate -Force
+winget source reset --force
 winget install Google.Chrome --accept-source-agreements
 winget install -e --id Adobe.Acrobat.Reader.64-bit --accept-source-agreements
 winget install -e --id Lenovo.SystemUpdate --accept-source-agreements
@@ -15,11 +16,12 @@ $WebClient.DownloadFile($DownloadUrl, $DownloadPath)
 Import-Module -Name PSWindowsUpdate
 Get-WindowsUpdate
 Install-WindowsUpdate Install-WindowsUpdate
-Restart-Computer -Force
+#Restart-Computer -Force
 
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
 #choco feature enable -n=allowGlobalConfirmation
 #choco install googlechrome --ignore-checksums
 #Invoke-WebRequest "https://get.teamviewer.com/6nsd5xz" -OutFile "C:\Temp\Netip.exe"
 #Move-Item -Path "C:\Temp\Netip.exe" -Destination ""$env:userprofile\desktop\netip.exe"" -Force
+
 
